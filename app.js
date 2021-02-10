ec9d63c20a0d39159c576c14a4ac6ddb8dba2657
benoitD03 <bens.92@hotmail.fr>
2021-02-10 11:21:30 +0100

    connexion à mongoDB + création model user

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://benoitD03:mnywc2pgtn@cluster0.tmkfm.mongodb.net/<dbname>?retryWrites=true&w=majority', 
+{ useNewUrlParser: true,
+  useUnifiedTopology: true })
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée'));
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(bodyParser.json());
+
+app.use((req, res) => {
+    res.json({ message: 'Requête bien reçue !' });
+});
+
+module.exports = app;
