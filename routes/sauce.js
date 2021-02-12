b26b8fc00469ebbc2402d34c019fffce098a4ec2
benoitD03 <bens.92@hotmail.fr>
2021-02-12 15:52:21 +0100

    suite requêtes + config multer

--- routes/sauce.js
+++ routes/sauce.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 const auth = require('../middleware/auth');
 const sauceController = require('../controllers/sauce');
+const multer = require('../middleware/multer-config');
 
-router.post('/', auth, sauceController.createSauce);
+router.post('/', auth, multer, sauceController.createSauce);
 router.get('/', auth, sauceController.getAllSauces);
+router.get('/:id', auth, sauceController.getOneSauce);
+router.put('/:id', auth, sauceController.modifySauce);
+router.delete('/:id', auth, sauceController.deleteSauce);
 
 module.exports = router;
